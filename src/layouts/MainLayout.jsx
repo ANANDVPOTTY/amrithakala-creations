0900ebb36978c5e82df9a79ac2af9fd404278677
Anand V Potty <anandvpotty@gmail.com>
2026-02-08 17:49:16 +0530

    Add Footer component with copyright information and styles

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 import { MainContainer, ContentWrapper } from "./MainLayout.styles";
 
 const MainLayout = () => {
@@ -10,6 +11,8 @@ const MainLayout = () => {
       <ContentWrapper component="main">
         <Outlet />
       </ContentWrapper>
+
+      <Footer />
     </MainContainer>
   );
 };
